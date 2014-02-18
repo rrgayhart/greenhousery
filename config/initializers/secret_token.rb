678a0bc888b85cd7a7fa4ea43c1966482c4d4957
Meeka <rrgayhart@gmail.com>
2014-02-18 14:18:30 -0700

    initial setup of rspec, postgres and rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Greenhousery::Application.config.secret_key_base = '886e7db635cde791944f8f2f9dba2e3ecfe070ddfbb002dda126e789f8ed06126dbcb419d526b197b466ac950122374f675481142e4888ef28ab6ac7cd99d117'
