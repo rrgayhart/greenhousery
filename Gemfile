864ae46cfbfb2121e81a2df0ad4205b1836b336b
Meeka <rrgayhart@gmail.com>
2014-02-20 16:37:46 -0700

    Adds gon to pass user latitude and longitude to the map js

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 gem 'rails', '4.0.2'
 gem 'geocoder'
 gem 'openlayers-rails'
+gem 'gon'
 
 group :testing, :development do
   gem 'rspec-rails'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    gon.latitude = @user.latitude
+    gon.longitude = @user.longitude
   end
 
   private
