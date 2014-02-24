9e193c49f3d2fb1e8b9aeb1c1df946ad19c2fc2a
Meeka <rrgayhart@gmail.com>
2014-02-24 14:29:10 -0700

    Adds a very large point creation method in ProjectArray

--- app/models/project_array.rb
+++ app/models/project_array.rb
@@ -3,6 +3,29 @@ class ProjectArray < ActiveRecord::Base
   has_many :points
   validates :project_id, presence: true
   after_validation :set_name
+
+  def set_points(args)
+    unless args[:length]
+      args[:length] = args[:square_foot] / args[:width]
+    end
+    unless args[:width]
+      args[:width] = args[:square_foot] /args[:length]
+    end
+    point1 = Point.new(x_coordinate: 0, y_coordinate: 0)
+    point2 = Point.new(x_coordinate: args[:width], y_coordinate: 0)
+    point3 = Point.new(x_coordinate: 0, y_coordinate: args[:length])
+    point4 = Point.new(x_coordinate: args[:width], y_coordinate: args[:length])
+    points = [point1, point2, point3, point4]
+    points.each do |point|
+      point.z_coordinate = 0
+      point.project_array_id = self.id
+      point.save
+    end
+  end
+
+  def one_point(args)
+    
+  end
   
 
   def set_name
--- spec/models/project_array_spec.rb
+++ spec/models/project_array_spec.rb
@@ -1,32 +1,46 @@
 require 'spec_helper'
 
 describe ProjectArray do
-  
-  context 'on creation' do
-    let(:user) { FactoryGirl.create(:user) }
+    let(:user) { FactoryGirl.create(:user) }  
     let(:project) { FactoryGirl.create(:project) }
 
+  context 'creating points' do
+    let(:project_array) { FactoryGirl.create(:project_array, project: project) }
+    
+    it 'should handle receiving length or width, shape and square foot' do
+      project_array.set_points({length: 10, shape:'rectange', square_foot: 200})
+      project_array.points.count.should eq(4)
+      project_array.points.first.x_coordinate.should eq(0)
+      project_array.points.first.y_coordinate.should eq(0)
+      project_array.points.last.x_coordinate.to_i.should eq(20)
+      project_array.points.last.y_coordinate.to_i.should eq(10)
+    end
+  end
+
+  context 'on creation' do
+
+    let(:project_array) { FactoryGirl.build(:project_array, project: project) }
+
     it 'should set_name on save' do
-      project_array = FactoryGirl.build(:project_array, project: project)
       project_array.name.should eq(nil)
       project_array.save
       project_array.name.should eq('Basic Solar Project Array')
     end
 
     it 'should not set_name if name exists' do
-      project_array = FactoryGirl.build(:project_array, project: project, name: 'My Array')
+      project_array.name = 'My Array'
       project_array.save
       project_array.name.should eq('My Array')
     end
 
     it 'should belong to a project' do
-      project_array = FactoryGirl.create(:project_array, project: project)
+      project_array.save
       project.project_arrays.first.should eq(project_array)
       project_array.project.should eq(project)
     end
 
     it 'should have points' do
-      project_array = FactoryGirl.create(:project_array, project: project)
+      project_array.save
       project_array.points.count.should eq(0)
     end
   end
