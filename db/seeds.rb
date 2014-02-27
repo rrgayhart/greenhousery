a2c4663ff1baaa90ba6b885132edf58782bc3680
Meeka <rrgayhart@gmail.com>
2014-02-27 14:00:02 -0700

    Seed Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+SolarModule.create(brand: 'ET Solar', model: 'Black Module Polycrystalline', nominal_wattage: 240, length: 39.06, width: 64.57, height: 1.58)
+
+SolarModule.create(brand: 'Hyundai', model: 'HiS-M225MF', nominal_wattage: 225, length: 38.70, width: 58.11, height: 1.38)
