86e0de843058f77aa41a5de8f2052fd7bfce2c17
Meeka <rrgayhart@gmail.com>
2014-02-27 11:19:47 -0700

    Fixes panel saving error

--- app/controllers/project_arrays_controller.rb
+++ app/controllers/project_arrays_controller.rb
@@ -51,7 +51,6 @@ class ProjectArraysController < ApplicationController
   def update_module
     new_params = module_update_params
     @project_array = ProjectArray.find(new_params[:id])
-    @project_array.update_attributes(project_array_params)
     @project_array.create_solar_module(module_update_params)
     redirect_to edit_layout_path(@project_array)
   end
@@ -82,7 +81,7 @@ class ProjectArraysController < ApplicationController
   private
   
   def project_array_params
-    params.permit(:name, :project_id, :sqft, :solar_module_id, :length, :width, :panel_spacing_side, :panel_spacing_back, :tilt_degrees, :horizontal, :kw_predicted, :kw_goal, :panel_count_predicted, :new_solar_module_brand, :new_solar_module_model, :new_solar_module_length, :new_solar_module_height, :new_solar_module_width, :new_solar_module_nominal_wattage)
+    params.require(:project_array).permit(:name, :project_id, :sqft, :solar_module_id, :length, :width, :panel_spacing_side, :panel_spacing_back, :tilt_degrees, :horizontal, :kw_predicted, :kw_goal, :panel_count_predicted, :new_solar_module_brand, :new_solar_module_model, :new_solar_module_length, :new_solar_module_height, :new_solar_module_width, :new_solar_module_nominal_wattage)
   end
 
   def module_update_params
